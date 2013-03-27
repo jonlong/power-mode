7944a977b5ff24585c9bfbc8a84ea382fae56f35
Jon Long <jon@istrategylabs.com>
2013-03-26 20:44:29 -0400

    Working scaffold for music and Hue lights

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  mpd: {
-    host: '127.0.0.1',
-    port: '6600'
-  }
-};
-
-module.exports = config;
--- server.js
+++ server.js
@@ -1,29 +1,42 @@
 var config = require('./config');
-var MPD = require('mpdsocket');
-var mpd = new MPD(config.mpd.host, config.mpd.port);
+var async = require('async');
+var log = require('./lib/log');
+var komponist = require('komponist');
+var Hue = require('node-hue-api').hue;
+var Room = require('./lib/Room').Room;
+var hueClient = new Hue.HueApi(config.hue.host, config.hue.user);
+var devCave;
+var devCaveOptions = {};
 
-function mpdSend(cmd) {
-  mpd.send(cmd,function(res) {
-    console.log(res);
+async.parallel([
+  // Connect to MPD
+  function(callback){
+    komponist.createConnection(config.mpd.port, config.mpd.host, function(err, client) {
+      devCaveOptions.mpd = client;
+      log(null, 'MPD client connected');
+      callback(null, client);
+    });
+  },
+  // Connect to Hue
+  function(callback){
+    hueClient.connect()
+    .then(function(result){
+      devCaveOptions.hue = hueClient;
+      log(null, 'Hue client connected');
+      callback(null, result);
+    })
+    .done();
+  }
+],
+// After all functions are complete
+function(err, results){
+  if (err) {
+    log(err);
+    return;
+  }
+
+  devCave = new Room(devCaveOptions);
+  devCave.initialize(function(){
+    devCave.changeHue();
   });
-}
-
-mpd.on('connect',function() {
-  console.log('MPD connected');
-  mpdSend('status');
-});
-
-mpd.on('disconnect', function() {
-  console.log('MPD disconnected');
-});
-
-mpd.on('error', function(err){
-  console.log('MPD error', err);
-});
-
-mpd.on('ready',function() {
-  mpdSend('status');
-  mpdSend('currentsong');
-  mpdSend('next');
-  mpdSend('playlist');
 });
