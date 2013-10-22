a4c2413d32def721df4ecb25a157a7ea8dd1c8ea
Jon Long <jon@linesandwaves.com>
2013-10-22 13:48:25 -0400

    Bit of refactoring after some time away—working build with node 10.2

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 var config = require('./config');
 var async = require('async');
 var log = require('./lib/log');
-var Hue = require('node-hue-api').hue;
+var Hue = require('node-hue-api').HueApi;
 var Room = require('./lib/Room').Room;
 var JohnnyFive = require('johnny-five');
 var komponist = require('komponist');
 var arduinoClient = new JohnnyFive.Board();
-var hueClient = new Hue.HueApi(config.hue.host, config.hue.user);
+var hueClient = new Hue(config.hue.host, config.hue.user);
 var devCaveOptions = {};
 var devCave;
+var button;
 
 async.parallel([
   // Connect to MPD
   function(callback){
     komponist.createConnection(config.mpd.port, config.mpd.host, function(err, client) {
+      if (err) {
+        log(err);
+        callback(err);
+      }
       devCaveOptions.mpd = client;
       log(null, 'MPD client connected');
       callback(null, client);
@@ -33,6 +38,10 @@ async.parallel([
   function(callback){
     devCaveOptions.arduino = arduinoClient;
     arduinoClient.on('ready', function(){
+      log(null, 'Arduino ready');
+
+      // (new JohnnyFive.Led(13)).strobe();
+      button = new JohnnyFive.Button(7);
       callback(null);
     });
   }
@@ -43,9 +52,24 @@ function(err, results){
     log(err);
     return;
   }
-
+  console.log('all functions done');
   devCave = new Room(devCaveOptions);
   devCave.initialize(function(){
-    devCave.activate();
+    var buttonEnabled = true;
+    log(null, 'initialized');
+
+    button.on('down', function(){
+      console.log('button pressed');
+
+      if (buttonEnabled) {
+        devCave.toggleSwitch();
+        buttonEnabled = false;
+
+        setTimeout(function(){
+          buttonEnabled = true;
+        }, config.buttonTimeout * 1000);
+      }
+
+    });
   });
 });
